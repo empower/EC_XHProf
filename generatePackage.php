92fc58838ad7c6e40496a0bc427205d8caf4f839
Bill Shupp <hostmaster@shupp.org>
2011-02-27 13:08:14 -0800

    * Added generatePackage.php and phpunit helper files * Add .gitignore so coverage directory is ignored

---
+++ generatePackage.php
@@ -0,0 +1,70 @@
+<?php
+
+error_reporting(E_ALL & ~E_DEPRECATED);
+
+require_once('PEAR/PackageFileManager2.php');
+
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+
+$packagexml = new PEAR_PackageFileManager2;
+
+$packagexml->setOptions(array(
+    'baseinstalldir'    => '/',
+    'simpleoutput'      => true,
+    'packagedirectory'  => './',
+    'filelistgenerator' => 'file',
+    'ignore'            => array(
+        'generatePackage.php',
+        'phpunit.xml',
+        'phpunit-bootstrap.php',
+        'coverage/'
+    ),
+    'dir_roles' => array(
+        'tests'     => 'test',
+        'examples'  => 'doc'
+    ),
+    'exceptions' => array(
+        'README.md' => 'doc',
+    ),
+));
+
+$packagexml->setPackage('EC_XHProf');
+$packagexml->setSummary('EC_XHProf is a simple class for managing XHProf runs.');
+$packagexml->setDescription(
+    'EC_XHProf is a simple class for managing XHProf runs and storing them according
+to virtual host ServerNames.  You can also clear out runs.  See the examples
+directory for examples.'
+);
+
+$packagexml->setChannel('empower.github.com/pirum');
+$packagexml->setAPIVersion('0.1.0');
+$packagexml->setReleaseVersion('0.1.0');
+
+$packagexml->setReleaseStability('alpha');
+
+$packagexml->setAPIStability('alpha');
+
+$packagexml->setNotes('
+* Initial release
+');
+$packagexml->setPackageType('php');
+$packagexml->addRelease();
+
+$packagexml->detectDependencies();
+
+$packagexml->addMaintainer('lead',
+                           'shupp',
+                           'Bill Shupp',
+                           'hostmaster@shupp.org');
+
+$packagexml->setLicense('New BSD License',
+                        'http://www.opensource.org/licenses/bsd-license.php');
+
+$packagexml->setPhpDep('5.0.0');
+$packagexml->setPearinstallerDep('1.4.0b1');
+$packagexml->addExtensionDep('optional', 'xhprof');
+
+$packagexml->generateContents();
+$packagexml->writePackageFile();
+
+?>
